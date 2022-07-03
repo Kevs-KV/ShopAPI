77d49f8f7e6bbb464e8ef8b17158741da2fa4940
Kirill Vashchenko <k.vaschhenko@gmail.com>
2022-07-03 19:49:39 +0300

    :construction: add product model

---
+++ project/backend/app/app/services/database/models/product.py
@@ -0,0 +1,20 @@
+import enum
+
+from sqlalchemy import Column, Integer, Identity, VARCHAR, Numeric, Enum, Text, DateTime, func
+
+from services.database.models.base import Base
+
+class ColorEnum(enum.Enum):
+    BLACK = 'Black'
+    WHITE = 'White'
+
+
+class Product(Base):
+    __tablename__ = 'product'
+
+    id = Column(Integer, Identity(always=True, cache=5), primary_key=True)
+    name = Column(VARCHAR(255), unique=True, index=True)
+    unit_price = Column(Numeric(precision=8), server_default="1")
+    size = Column(Enum(ColorEnum))
+    description = Column(Text, default=None, nullable=True)
+    created_at = Column(DateTime(), server_default=func.now())
