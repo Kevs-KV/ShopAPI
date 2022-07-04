f2ef629c452aadaed0e7e8f4b7271bebbecb84c6
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-04 15:27:48 +0300

    :fire: delete color field

--- project/backend/app/app/services/database/models/product.py
+++ project/backend/app/app/services/database/models/product.py
@@ -15,7 +15,6 @@ class Product(Base):
 
     id = Column(Integer, Identity(always=True, cache=5), primary_key=True)
     name = Column(VARCHAR(255), unique=True, index=True)
-    color = Enum(ColorEnum)
     unit_price = Column(Numeric(precision=8), server_default="1")
     description = Column(Text, default=None, nullable=True)
     created_at = Column(DateTime(), server_default=func.now())
