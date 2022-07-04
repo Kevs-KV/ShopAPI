4997e2168d3520a7a0422e13a2730bbcfeaf4432
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-04 15:23:42 +0300

    :construction: add post request for create product

--- project/backend/app/app/api/v1/endpoints/product.py
+++ project/backend/app/app/api/v1/endpoints/product.py
@@ -1,3 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.responses import Response
+
+from services.database.repositories.product_repository import ProductRepository
+from services.database.schemas.product import ProductDTO, ProductBodySpec
 
 router = APIRouter()
+
+
+@router.post('/product/create/')
+async def product_create(product: ProductDTO = ProductBodySpec.item, product_crud=Depends(ProductRepository)):
+    await product_crud.add_product(**product.dict(exclude_unset=True, exclude_none=True))
+    return Response(status_code=201)
