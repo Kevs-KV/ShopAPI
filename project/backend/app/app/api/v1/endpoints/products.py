93716d9155ad86e771ee25a04354af9a49cfb771
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-05 16:41:03 +0300

    :passport_control: add product creation permission only to super user

--- project/backend/app/app/api/v1/endpoints/products.py
+++ project/backend/app/app/api/v1/endpoints/products.py
@@ -1,16 +1,20 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import Response
 
+from services.database.models.user import User
 from services.database.repositories.product_repository import ProductRepository
 from services.database.schemas.product import ProductDTO, ProductBodySpec
+from services.security.oauth import get_current_active_superuser
 
 router = APIRouter()
 
 
 @router.post('/create/')
-async def product_create(product: ProductDTO = ProductBodySpec.item, product_crud=Depends(ProductRepository)):
-    await product_crud.add_product(**product.dict(exclude_unset=True, exclude_none=True))
-    return Response(status_code=201)
+async def product_create(product: ProductDTO = ProductBodySpec.item, product_crud=Depends(ProductRepository),
+                         current_user: User = Depends(get_current_active_superuser)):
+    if current_user.is_active:
+        await product_crud.add_product(**product.dict(exclude_unset=True, exclude_none=True))
+        return Response(status_code=201)
 
 
 @router.get('/all/')
