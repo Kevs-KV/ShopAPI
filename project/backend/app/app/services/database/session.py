3c221d17023ea36f89327b568e4feacdbe88a226
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-07 11:29:23 +0300

    :art: add the DatabaseComponents class to inject into the builder

--- project/backend/app/app/services/database/session.py
+++ project/backend/app/app/services/database/session.py
@@ -1,15 +1,26 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from config.settings import settings
-
-engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True, future=True)
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+from services.database.models.base import Base
 
 
-async def get_session() -> AsyncSession:
-    async_session = sessionmaker(
-        engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with async_session() as session:
-        yield session
+class DatabaseComponents:
+
+    def __init__(self, connection_uri: str, **engine_kwargs) -> None:
+        self.__engine_kwargs = engine_kwargs or {}
+        self.engine = create_async_engine(url=connection_uri, **self.__engine_kwargs)
+        self.sessionmaker = sessionmaker(
+            self.engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
+
+        )
+
+    async def init_models(self):
+        session = self.engine
+        async with session.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+        await session.dispose()
+
+    async def get_session(self) -> AsyncSession:
+        async with self.sessionmaker as session:
+            yield session
