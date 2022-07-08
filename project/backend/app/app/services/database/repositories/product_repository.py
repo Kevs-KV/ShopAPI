22cff14f7161c50dbb7a77d9b5c6ee7487ae73a1
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-08 15:18:12 +0300

    :recycle: optimization product repository

--- project/backend/app/app/services/database/repositories/product_repository.py
+++ project/backend/app/app/services/database/repositories/product_repository.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSessionTransaction
 from services.database.models.product import Product
 from services.database.repositories.base import Base, ASTERISK
 from services.database.schemas.product import ProductDTO
+from utils.database_utils import filter_payload
 
 Model = typing.TypeVar("Model")
 TransactionContext = typing.AsyncContextManager[AsyncSessionTransaction]
@@ -17,37 +18,21 @@ class ProductRepository(Base):
     model = Product
     DTO = ProductDTO
 
-    async def add_product(self,
+    async def add_product(self, *,
                           name: str,
                           unit_price: typing.Union[float, Decimal],
+                          product_id: typing.Optional[int] = None,
                           description: typing.Optional[str] = None,
-                          created_at: typing.Optional[datetime] = None,
+                          created_at: typing.Optional[datetime] = None
                           ) -> Model:
-        product = Product(name=name, unit_price=unit_price, description=description, created_at=created_at)
-        async with self._transaction:
-            session = self.get_session()
-            session.add(product)
-            await session.commit()
-            await session.refresh(product)
-            return product
+        payload = filter_payload(locals())
+        return await self._insert(**payload)
 
     async def all_product(self) -> typing.List[Model]:
-        async with self._transaction:
-            session = self.get_session()
-            result = await session.execute(select(self.model))
-            return result.scalars().all()
+        return await self._select_all()
 
-    async def search_product(self, value: str) -> typing.List[Model]:
-        async with self._transaction:
-            session = self.get_session()
-            result = await session.execute(
-                select(self.model).order_by(self.model.name).filter(self.model.name == value))
-            return result.scalars().all()
+    async def search_product_by_name(self, value: str) -> typing.List[Model]:
+        return await self._filter(self.model.name, value)
 
-    async def delete_product(self, product_id: int) -> typing.List[Model]:
-        async with self._transaction:
-            session = self.get_session()
-            stmt = delete(self.model).where(self.model.id == product_id).returning(ASTERISK)
-            result = (await session.execute(stmt)).mappings().all()
-            await session.commit()
-        return list(map(self._convert_to_model, result))
+    async def delete_product(self, product_id: int) -> Model:
+        return await self._delete(self.model.id, product_id)
