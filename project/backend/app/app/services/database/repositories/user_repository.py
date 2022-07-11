6f7da635058ba728ee83f448e8b345510e9703a8
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-11 15:29:38 +0300

    :recycle: optimization user repository

--- project/backend/app/app/services/database/repositories/user_repository.py
+++ project/backend/app/app/services/database/repositories/user_repository.py
@@ -7,53 +7,29 @@ from services.database.models.user import User
 from services.database.repositories.base import Base, Model
 from services.database.schemas.user import UserCreate
 from services.security.jwt import verify_password, get_password_hash
+from utils.database_utils import filter_payload
 
 
 class UserRepository(Base):
     model = User
 
     async def get_user(self, name: str) -> Model:
-        async with self._transaction:
-            session = self.get_session()
-            result = await session.execute(select(self.model).
-                                           order_by(self.model.name).
-                                           filter(self.model.name == name))
-            return typing.cast(Model, result.scalars().first())
+        return await self._select_one(self.model.name == name)
 
     async def get_user_id(self, id: int) -> Model:
-        async with self._transaction:
-            session = self.get_session()
-            result = await session.execute(select(self.model).
-                                           order_by(self.model.id).
-                                           filter(self.model.id == id))
-
-            return typing.cast(Model, result.scalars().first())
+        return await self._select_one(self.model.id == id)
 
     async def get_by_email(self, email: str) -> Model:
-        async with self._transaction:
-            session = self.get_session()
-            result = await session.execute(select(self.model).
-                                           order_by(self.model.email).
-                                           filter(self.model.email == email))
-            return typing.cast(Model, result.scalars().first())
+        return await self._select_one(self.model.email == email)
 
-    async def create_user(self, obj_in: UserCreate) -> User:
-        user = User(
-            email=obj_in.email,
-            hashed_password=get_password_hash(obj_in.password),
-            full_name=obj_in.full_name,
-            is_superuser=obj_in.is_superuser,
-        )
-        async with self._transaction:
-            session = self.get_session()
-            session.add(user)
-            await session.commit()
-            await session.refresh(user)
-            return user
+    async def create_user(self, obj_in: UserCreate) -> Model:
+        payload = obj_in.__dict__
+        payload['hashed_password'] = get_password_hash(obj_in.password)
+        del payload['password']
+        return await self._insert(**payload)
 
     async def authenticate(self, *, email: str, password: str) -> Optional[User]:
-        user = await self.get_by_email(email=email)
-        user = user
+        user = await self.get_by_email(email)
         if not user:
             return None
         if not verify_password(password, user.hashed_password):
