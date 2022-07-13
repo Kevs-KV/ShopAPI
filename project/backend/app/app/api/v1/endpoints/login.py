7c5cc708585dc1036a599c54f1c6df9226dec753
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-13 15:57:39 +0300

    :construction: add new login api

--- project/backend/app/app/api/v1/endpoints/login.py
+++ project/backend/app/app/api/v1/endpoints/login.py
@@ -1,32 +1,26 @@
-from datetime import timedelta
 from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
+from starlette import status
 
-from api.v1.dependencies.database_marker import ProductRepositoryDependencyMarker, UserRepositoryDependencyMarker
-from config.settings import settings
-from services.database.repositories.product_repository import ProductRepository
-from services.database.repositories.user_repository import UserRepository
-from services.database.schemas.token import Token
-from services.security.jwt import create_access_token
+from api.v1.dependencies.security import JWTAuthenticationMarker
+from services.security.jwt import JWTAuthenticationService
 
 router = APIRouter()
 
 
-@router.post("/login/access-token", response_model=Token)
+@router.post("/login/access-token")
 async def login_access_token(
-        user_crud: UserRepository = Depends(UserRepositoryDependencyMarker), form_data: OAuth2PasswordRequestForm = Depends()
+        auth: JWTAuthenticationService = Depends(JWTAuthenticationMarker),
+        form_data: OAuth2PasswordRequestForm = Depends()
 ) -> Any:
-    user = await user_crud.authenticate(email=form_data.username, password=form_data.password)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
-    elif not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    return {
-        "access_token": create_access_token(
-            user.id, expires_delta=access_token_expires
-        ),
-        "token_type": "bearer",
-    }
+    try:
+        access_token = await auth.authenticate_user(form_data)
+        return {"access_token": access_token, "token_type": "bearer"}
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='no auth',
+            headers={"WWW-Authenticate": "Bearer"},
+        )
