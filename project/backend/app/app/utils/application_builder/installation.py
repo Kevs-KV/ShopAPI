666605504afb2e07b18b3b90ffd9327d87e85f88
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-14 16:37:47 +0300

    :construction: add configure openapi and JWT security service

--- project/backend/app/app/utils/application_builder/installation.py
+++ project/backend/app/app/utils/application_builder/installation.py
@@ -1,19 +1,19 @@
 from typing import no_type_check
 
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 from passlib.context import CryptContext
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from api.v1.dependencies.database_marker import UserRepositoryDependencyMarker, ProductRepositoryDependencyMarker
-from api.v1.dependencies.security import JWTAuthenticationMarker
+from api.v1.dependencies.security import JWTAuthenticationMarker, JWTSecurityMarker
 from config.settings import settings
 from middlewares.process_time_middleware import add_process_time_header
 from services.database.repositories.product_repository import ProductRepository
 from services.database.repositories.user_repository import UserRepository
 from services.database.session import DatabaseComponents
-from services.security.jwt import JWTAuthenticationService
+from services.security.jwt import JWTAuthenticationService, JWTSecurityService
 from utils.password_hashing import PasswordHasher
 
 
@@ -23,6 +23,17 @@ class DependencyApplicationBuilder:
         self.app = app
         self._settings = config
 
+    def configure_openapi_schema(self) -> None:
+        self._openapi_schema = get_openapi(
+            title="Kevs",
+            version="0.0.1",
+            description="This is a very custom OpenAPI schema",
+            routes=self.app.routes,
+        )
+        self._openapi_schema["info"]["x-logo"] = {
+            "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+        }
+        self.app.openapi_schema = self._openapi_schema
 
     @no_type_check
     def setup_middlewares(self):
@@ -45,7 +56,14 @@ class DependencyApplicationBuilder:
                 password_hasher=password_hasher,
                 secret_key=self._settings.SECRET_KEY,
                 algorithm="HS256",
-                token_expires_in_minutes=self._settings.ACCESS_TOKEN_EXPIRE_MINUTES)})
+                token_expires_in_minutes=self._settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+            JWTSecurityMarker: lambda: JWTSecurityService(password_hasher=password_hasher,
+                                                          user_crud=UserRepository(db_components.sessionmaker,
+                                                                                   password_hasher),
+                                                          algorithm="HS256",
+                                                          secret_key=self._settings.SECRET_KEY)}
+
+        )
 
 
 class ApplicationBuilder:
@@ -62,6 +80,7 @@ class ApplicationBuilder:
         self._build = new_builder
 
     def build_app(self) -> FastAPI:
+        self._build.configure_openapi_schema()
         self._build.setup_middlewares()
         self._build.configure_application_state()
         return self._build.app
