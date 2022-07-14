0b7e70b2e397e53fc28b840f246e8f850bea7668
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-14 16:29:41 +0300

    :passport_control: add JWT security service and scopes jwt tokens

--- project/backend/app/app/services/security/jwt.py
+++ project/backend/app/app/services/security/jwt.py
@@ -1,10 +1,19 @@
 from datetime import datetime, timedelta
 from typing import NewType, Dict, Any
 
-import jwt
+from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import SecurityScopes
+from jose import jwt
+from pydantic import ValidationError
+from starlette import status
 
-ALGORITHM = "HS256"
+from api.v1.dependencies.security import JWTSecurityMarker
+from services.database.models.user import User
+from services.database.repositories.user_repository import UserRepository
+from services.database.schemas.token import TokenPayload
+from services.security.oauth import reusable_oauth2
+from utils.password_hashing import PasswordHasher
 
 JWTToken = NewType("JWTToken", str)
 
@@ -36,3 +45,58 @@ class JWTAuthenticationService:
         }
         filtered_payload = {k: v for k, v in token_payload.items() if v is not None}
         return jwt.encode(filtered_payload, self._secret_key, algorithm=self._algorithm)
+
+
+class JWTSecurityService:
+
+    def __init__(self, user_crud: UserRepository,
+                 password_hasher: PasswordHasher, secret_key: str, algorithm: str):
+        self._secret_key = secret_key
+        self._algorithm = algorithm
+        self._user_crud = user_crud
+        self._password_hasher = password_hasher
+
+    async def __call__(self, security_scopes: SecurityScopes, jwt_token: str = Depends(reusable_oauth2)) -> User:
+        if jwt_token is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='error call',
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        token_payload = self._decode_token(token=jwt_token)
+        for scope in security_scopes.scopes:
+            if scope not in token_payload.scopes:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail='erorr scope',
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+        return await self._retrieve_user_or_raise_exception(token_payload.sub)
+
+    def _decode_token(self, token: str) -> TokenPayload:
+        try:
+            payload = jwt.decode(token, self._secret_key, algorithms=[self._algorithm])
+            return TokenPayload(sub=payload["sub"], scopes=payload.get("scopes", []))
+        except (jwt.JWTError, ValidationError):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='erorr decode_token',
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    async def _retrieve_user_or_raise_exception(self, username: str) -> User:
+        if user := await self._user_crud.get_by_email(email=username):
+            return user
+
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='error raise_exception',
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+
+class JWTSecurityHead:
+
+    async def __call__(self, security_scopes: SecurityScopes, jwt_token: str = Depends(reusable_oauth2),
+                       service: JWTSecurityService = (Depends(JWTSecurityMarker))):
+        return await service(jwt_token=jwt_token, security_scopes=security_scopes)
