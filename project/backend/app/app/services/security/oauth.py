23e777ea731a32f680b2130627cefb05f336b92f
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-14 16:31:11 +0300

    :fire: delete everything related to jwt

--- project/backend/app/app/services/security/oauth.py
+++ project/backend/app/app/services/security/oauth.py
@@ -1,53 +1,7 @@
-from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
-from pydantic import ValidationError
-from starlette import status
 
-from api.v1.dependencies.database_marker import UserRepositoryDependencyMarker
 from config.settings import settings
-from services.database.models.user import User
-from services.database.repositories.user_repository import UserRepository
-from services.database.schemas.token import TokenPayload
-from services.security.jwt import ALGORITHM
 
 reusable_oauth2 = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/login/access-token"
 )
-
-
-async def get_current_user(
-        user_crud: UserRepository = Depends(UserRepositoryDependencyMarker), token: str = Depends(reusable_oauth2)
-) -> User:
-    try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[ALGORITHM]
-        )
-        token_data = TokenPayload(**payload)
-    except (jwt.JWTError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )
-    user = await user_crud.get_user_id(id=token_data.sub)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-
-async def get_current_active_user(
-        current_user: User = Depends(get_current_user),
-) -> User:
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-async def get_current_active_superuser(
-        current_user: User = Depends(get_current_user),
-) -> User:
-    if not current_user.is_superuser:
-        raise HTTPException(
-            status_code=400, detail="The user doesn't have enough privileges"
-        )
-    return current_user
