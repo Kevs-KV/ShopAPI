84e9f8101e49454fe3cb809d0dba45e6021dac95
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-15 13:55:55 +0300

    :construction: add scopes in password bearer

--- project/backend/app/app/services/security/oauth.py
+++ project/backend/app/app/services/security/oauth.py
@@ -3,5 +3,7 @@ from fastapi.security import OAuth2PasswordBearer
 from config.settings import settings
 
 reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token", scopes={
+        'user': 'Only users', 'admin': 'Only admins'
+    }
 )
