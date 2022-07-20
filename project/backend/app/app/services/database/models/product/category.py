dc549c4e764b4ae6dbaa8c3175d40aedae78cd09
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-07-20 16:21:49 +0300

    :bug: fix back_populates in model

--- project/backend/app/app/services/database/models/product/category.py
+++ project/backend/app/app/services/database/models/product/category.py
@@ -7,4 +7,4 @@ from services.database.models.base import Base
 class Category(Base):
     id = Column(Integer, Identity(always=True, cache=5), primary_key=True)
     name = Column(VARCHAR(255), unique=True, index=True)
-    products = relationship('Product', backref="category", lazy=True)
+    products = relationship('Product', back_populates="category", lazy=True)
