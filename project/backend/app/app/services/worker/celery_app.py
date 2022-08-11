d3ab5bfb1f6088af9c97ec8f450a10eb2ac03d9c
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-08-11 16:17:51 +0300

    :poop: in the process of fixing bugs

--- project/backend/app/app/services/worker/celery_app.py
+++ project/backend/app/app/services/worker/celery_app.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from celery import Celery
 from celery.schedules import crontab
 
@@ -8,6 +10,7 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
-celery_app = Celery("worker", broker="amqp://guest:guest@rabbitmq:5672//")
+celery_app = Celery("worker", broker="amqp://rabbitmq:5672")
+celery_app.conf.update(BROKER_URL="amqp://rabbitmq:5672")
 celery_app.config_from_object(CELERY_BEAT_SCHEDULE, namespace="CELERY")
 celery_app.autodiscover_tasks()
--- project/backend/app/app/services/worker/celery_test.py
+++ project/backend/app/app/services/worker/celery_test.py
@@ -6,11 +6,10 @@ from app.services.worker.celery_app import celery_app
 logger = get_task_logger(__name__)
 
 
-@shared_task
-def sample_task():
-    logger.info("The sample task just ran.")
 
-
-@celery_app.task(acks_late=True)
-def test_celery() -> str:
-    return f"test task return True"
+@celery_app.task()
+def test_celery_start(*args) -> str:
+    try:
+        return f"{args}"
+    except:
+        return 'test'
