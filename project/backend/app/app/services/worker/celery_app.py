af35bcf5f0b6e0714bdce3305bf4638e0dae4dd4
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-08-15 17:00:10 +0300

    :recycle: refactor celery application

--- project/backend/app/app/services/worker/celery_app.py
+++ project/backend/app/app/services/worker/celery_app.py
@@ -1,14 +1,17 @@
 from celery import Celery
 
-celery_app = Celery("worker", broker="amqp://rabbitmq:5672", include=["app.services.worker.tasks"])
 
-
-class Config:
+class CeleryConfig:
     enable_utc = True
     timezone = 'Europe/Minsk'
     broker_url = "amqp://rabbitmq:5672"
     result_backend = "rpc://"
 
 
-celery_app.config_from_object(Config)
-celery_app.autodiscover_tasks()
+def celery_application() -> Celery:
+    celery_app = Celery("worker", broker="amqp://rabbitmq:5672", include=["app.services.worker.tasks"])
+    celery_app.config_from_object(CeleryConfig)
+    return celery_app
+
+
+celery_app = celery_application()
