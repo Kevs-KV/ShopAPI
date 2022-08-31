966b56011cde6e0935f3ba33cf0485ec95245bd7
Kirill Vashchenko <k.vashhenko@gmail.com>
2022-08-31 12:31:23 +0300

    :sparkles: add use gunicorn with unicorn and logging

--- project/backend/app/app/__main__.py
+++ project/backend/app/app/__main__.py
@@ -1,11 +1,10 @@
-from typing import Any
-
-import uvicorn
 from fastapi import FastAPI
 
 from app.api.v1.api import api_router
 from app.config.settings import settings
 from app.utils.application_builder.installation import ApplicationBuilder, DependencyApplicationBuilder
+from app.utils.gunicorn_app import StandaloneApplication
+from app.utils.logging import configure_logging, LoggingConfig
 
 
 def app_factory() -> FastAPI:
@@ -15,10 +14,20 @@ def app_factory() -> FastAPI:
     return app
 
 
-def run_application(**kwargs: Any) -> None:
+def run_application() -> None:
     app = app_factory()
-    uvicorn.run(app, **kwargs)
+    stdlib_logconfig_dict = configure_logging(LoggingConfig())
+    options = {
+        "bind": "%s:%s" % ("0.0.0.0", 8080),
+        "worker_class": "uvicorn.workers.UvicornWorker",
+        "reload": True,
+        "disable_existing_loggers": False,
+        "preload_app": True,
+        "logconfig_dict": stdlib_logconfig_dict
+    }
+    gunicorn_app = StandaloneApplication(app, options)
+    gunicorn_app.run()
 
 
 if __name__ == "__main__":
-    run_application(host="0.0.0.0", port=8080)
+    run_application()
